f20112a988504844426843861506e0c5fa8ecfca
cikelino <g.sergio@majorbit.com>
2026-09-07 15:23:34 +0200

    Fix build4.6

--- app/page.js
+++ app/page.js
@@ -149,7 +149,7 @@ export default function Home() {
                   type="email"
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
-                  placeholder="m.rossi@gmail.com"
+                  placeholder="m.rossi@majorbit.com"
                   required
                 />
               </div>
